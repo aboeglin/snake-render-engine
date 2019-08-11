104b682454ca7735d4a54020457fba229f3161f2
Arnaud BOEGLIN <a.boeglin67@gmail.com>
2019-08-11 23:30:32 +0200

    First drawing of a rect - WIP

--- render-engine/src/index.js
+++ render-engine/src/index.js
@@ -1,6 +1,6 @@
 const { curry } = require("ramda");
 
-const rect = curry((props, children = []) => {
+const Rect = curry((props, children = []) => {
   return {
     type: "RECTANGLE",
     position: {
@@ -13,20 +13,110 @@ const rect = curry((props, children = []) => {
   };
 });
 
-const circle = () => {};
-
 const init = gl => {
+  gl.viewport(0, 0, 128, 128);
   gl.clearColor(0.0, 0.0, 0.0, 1.0);
   gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 };
 
-const render = root => {
-  return root();
+const FRAGMENT_SHADER = `
+#ifdef GL_ES
+precision highp float;
+#endif
+
+uniform vec4 uColor;
+
+void main() {
+gl_FragColor = uColor;
+}
+`;
+
+const VERTEX_SHADER = `
+attribute vec2 aVertexPosition;
+
+void main() {
+gl_Position = vec4(aVertexPosition, 0.0, 1.0);
+}
+`;
+
+const render = (gl, scene) => {
+  var vs = gl.createShader(gl.VERTEX_SHADER);
+  gl.shaderSource(vs, VERTEX_SHADER);
+  gl.compileShader(vs);
+
+  var fs = gl.createShader(gl.FRAGMENT_SHADER);
+  gl.shaderSource(fs, FRAGMENT_SHADER);
+  gl.compileShader(fs);
+
+  program = gl.createProgram();
+  gl.attachShader(program, vs);
+  gl.attachShader(program, fs);
+  gl.linkProgram(program);
+
+  if (!gl.getShaderParameter(vs, gl.COMPILE_STATUS))
+    console.log(gl.getShaderInfoLog(vs));
+
+  if (!gl.getShaderParameter(fs, gl.COMPILE_STATUS))
+    console.log(gl.getShaderInfoLog(fs));
+
+  if (!gl.getProgramParameter(program, gl.LINK_STATUS))
+    console.log(gl.getProgramInfoLog(program));
+
+  const vertices = new Float32Array([
+    scene.position.x,
+    scene.position.y,
+
+    scene.position.x,
+    scene.position.y + scene.position.width,
+
+    scene.position.x + scene.position.width,
+    scene.position.y + scene.position.width,
+
+    scene.position.x + scene.position.width,
+    scene.position.y + scene.position.width,
+
+    scene.position.x + scene.position.width,
+    scene.position.y,
+
+    scene.position.x,
+    scene.position.y
+  ]);
+
+  // const aspect = 1;
+
+  // var vertices = new Float32Array([
+  //   -0.5,
+  //   0.5 * aspect,
+  //   0.5,
+  //   0.5 * aspect,
+  //   0.5,
+  //   -0.5 * aspect, // Triangle 1
+  //   -0.5,
+  //   0.5 * aspect,
+  //   0.5,
+  //   -0.5 * aspect,
+  //   -0.5,
+  //   -0.5 * aspect // Triangle 2
+  // ]);
+
+  const vbuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, vbuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
+
+  gl.useProgram(program);
+
+  program.uColor = gl.getUniformLocation(program, "uColor");
+  gl.uniform4fv(program.uColor, [1.0, 1.0, 0.0, 1.0]);
+
+  program.aVertexPosition = gl.getAttribLocation(program, "aVertexPosition");
+  gl.enableVertexAttribArray(program.aVertexPosition);
+  gl.vertexAttribPointer(program.aVertexPosition, 2, gl.FLOAT, false, 0, 0);
+
+  gl.drawArrays(gl.TRIANGLES, 0, 6);
 };
 
 module.exports = {
   init,
   render,
-  rect,
-  circle
+  Rect
 };
--- render-engine/src/index.spec.js
+++ render-engine/src/index.spec.js
@@ -7,115 +7,119 @@ expect.extend({ toMatchImageSnapshot });
 
 const SRE = require("./index");
 
+const VIEWPORT_WIDTH = 128;
+const VIEWPORT_HEIGHT = 128;
+
 describe("render engine", () => {
-  test("It should have a render function", () => {
-    expect(typeof SRE.render).toBe("function");
-  });
-
-  test("It should have a rect function", done => {
-    SRE.render(props => {
-      expect(typeof SRE.rect).toBe("function");
-      done();
+  describe("render", () => {
+    test("It should have a render function", () => {
+      expect(typeof SRE.render).toBe("function");
     });
-  });
 
-  test("The rect function provided by render should build a rect", () => {
-    const expected = {
-      type: "RECTANGLE",
-      position: { x: 0, y: 0, z: 0, width: 5 },
-      children: []
-    };
+    test("It should be able to render rects", done => {
+      const gl = makeGL(VIEWPORT_WIDTH, VIEWPORT_HEIGHT);
+      const rect = SRE.Rect({ x: 0, y: 0, z: 0, width: 1 });
+      SRE.init(gl);
+      SRE.render(gl, rect);
 
-    const actual = SRE.render(props =>
-      SRE.rect({ x: 0, y: 0, z: 0, width: 5 })
-    );
-
-    expect(actual).toEqual(expected);
-  });
-
-  test("The rect function should take children", () => {
-    const expected = {
-      type: "RECTANGLE",
-      position: { x: 0, y: 0, z: 0, width: 5 },
-      children: [
-        {
-          type: "RECTANGLE",
-          position: { x: 0, y: 0, z: 0, width: 5 },
-          children: []
-        }
-      ]
-    };
-
-    const actual = SRE.render(props =>
-      SRE.rect({ x: 0, y: 0, z: 0, width: 5 }, [
-        SRE.rect({ x: 0, y: 0, z: 0, width: 5 })
-      ])
-    );
-
-    expect(actual).toEqual(expected);
-  });
-
-  test("The render function should be able to render custom logic nodes", () => {
-    const expected = {
-      type: "RECTANGLE",
-      position: { x: 1, y: 1, z: 1, width: 5 },
-      children: []
-    };
-
-    const customElement = props =>
-      SRE.rect({
-        x: props.x / 2,
-        y: props.y / 2,
-        z: props.z / 2,
-        width: props.width / 2
+      makeSnapshot(gl, VIEWPORT_WIDTH, VIEWPORT_HEIGHT).then(ss => {
+        expect(ss).toMatchImageSnapshot();
+        gl.getExtension("STACKGL_destroy_context").destroy();
+        done();
       });
-
-    const actual = SRE.render(props =>
-      customElement({ x: 2, y: 2, z: 2, width: 10 })
-    );
-
-    expect(actual).toEqual(expected);
-  });
-
-  test("It should have a circle function", done => {
-    SRE.render(props => {
-      expect(typeof SRE.circle).toBe("function");
-      done();
     });
   });
 
-  test("rect should take a props param that bundles all info about the rect", () => {
-    const expected = {
-      type: "RECTANGLE",
-      position: { x: 2, y: 2, z: 0, width: 5 },
-      children: []
-    };
+  describe("Rect", () => {
+    test("It should have a Rect function", () => {
+      expect(typeof SRE.Rect).toBe("function");
+    });
 
-    const actual = SRE.render(props =>
-      SRE.rect({ x: 2, y: 2, z: 0, width: 5 })
-    );
+    test("The Rect function should build a rect js object", () => {
+      const expected = {
+        type: "RECTANGLE",
+        position: { x: 0, y: 0, z: 0, width: 5 },
+        children: []
+      };
 
-    expect(actual).toEqual(expected);
+      const actual = SRE.Rect({ x: 0, y: 0, z: 0, width: 5 });
+
+      expect(actual).toEqual(expected);
+    });
+
+    test("The Rect function should take children", () => {
+      const expected = {
+        type: "RECTANGLE",
+        position: { x: 0, y: 0, z: 0, width: 5 },
+        children: [
+          {
+            type: "RECTANGLE",
+            position: { x: 0, y: 0, z: 0, width: 5 },
+            children: []
+          }
+        ]
+      };
+
+      const actual = SRE.Rect({ x: 0, y: 0, z: 0, width: 5 }, [
+        SRE.Rect({ x: 0, y: 0, z: 0, width: 5 })
+      ]);
+
+      expect(actual).toEqual(expected);
+    });
+
+    test("The node functions should be able to render custom logic nodes", () => {
+      const expected = {
+        type: "RECTANGLE",
+        position: { x: 1, y: 1, z: 1, width: 5 },
+        children: []
+      };
+
+      const customElement = props =>
+        SRE.Rect({
+          x: props.x / 2,
+          y: props.y / 2,
+          z: props.z / 2,
+          width: props.width / 2
+        });
+
+      const actual = customElement({ x: 2, y: 2, z: 2, width: 10 });
+
+      expect(actual).toEqual(expected);
+    });
+
+    test("Rect should take a props param that bundles all info about the rect", () => {
+      const expected = {
+        type: "RECTANGLE",
+        position: { x: 2, y: 2, z: 0, width: 5 },
+        children: []
+      };
+
+      const actual = SRE.Rect({ x: 2, y: 2, z: 0, width: 5 });
+
+      expect(actual).toEqual(expected);
+    });
   });
 
   describe("init", () => {
     test("init should set a black background", done => {
-      const gl = makeGL(128, 128);
+      const gl = makeGL(VIEWPORT_WIDTH, VIEWPORT_HEIGHT);
+
       SRE.init(gl);
 
-      makeSnapshot(gl).then(ss => {
+      makeSnapshot(gl, VIEWPORT_WIDTH, VIEWPORT_HEIGHT).then(ss => {
         expect(ss).toMatchImageSnapshot();
+        gl.getExtension("STACKGL_destroy_context").destroy();
         done();
       });
     });
   });
 });
 
-const makeSnapshot = gl => {
+const makeSnapshot = (gl, width, height) => {
   return new Promise((resolve, reject) => {
-    const canvasPixels = new Uint8Array(128 * 128 * 4);
-    gl.readPixels(0, 0, 128, 128, gl.RGBA, gl.UNSIGNED_BYTE, canvasPixels);
-    var nd = ndArray(canvasPixels, [128, 128, 4]);
+    const canvasPixels = new Uint8Array(width * height * 4);
+    gl.readPixels(0, 0, width, height, gl.RGBA, gl.UNSIGNED_BYTE, canvasPixels);
+    var nd = ndArray(canvasPixels, [width, height, 4]);
 
     const chunks = [];
     const reader = savePixels(nd, ".png");
