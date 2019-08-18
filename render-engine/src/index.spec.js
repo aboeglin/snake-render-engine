d3fc1910607015fd836226a6446351a5904dd3f0
Arnaud BOEGLIN <a.boeglin67@gmail.com>
2019-08-18 20:03:30 +0200

    Clean up tests

--- render-engine/src/index.spec.js
+++ render-engine/src/index.spec.js
@@ -14,42 +14,41 @@ const Rect = require("./Rect");
 const VIEWPORT_WIDTH = 128;
 const VIEWPORT_HEIGHT = 128;
 
-const gl = makeGL(VIEWPORT_WIDTH, VIEWPORT_HEIGHT, {
-  preserveDrawingBuffer: true
-});
-
-Array(100)
-  .fill({})
-  .forEach(_ =>
-    makeGL(VIEWPORT_WIDTH, VIEWPORT_HEIGHT, {
-      preserveDrawingBuffer: true
-    })
-  );
+let gl = null;
+let gl256 = null;
 
 describe("render engine", () => {
-  describe("render", () => {
-    test("It should have a render function", () => {
-      expect(typeof SRE.render).toBe("function");
-    });
+  beforeAll(() => {
+    gl = makeGL(VIEWPORT_WIDTH, VIEWPORT_HEIGHT);
 
-    test("It should be able to render rects", done => {
-      const Scene = props =>
-        Rect(
-          {
-            x: 32,
-            y: 32,
-            z: 0,
-            width: 64
-          },
-          []
-        );
-      SRE.init(gl, VIEWPORT_WIDTH, VIEWPORT_HEIGHT, Scene);
+    gl256 = makeGL(VIEWPORT_WIDTH * 2, VIEWPORT_HEIGHT * 2);
+  });
 
-      makeSnapshot(gl, VIEWPORT_WIDTH, VIEWPORT_HEIGHT).then(ss => {
-        expect(ss).toMatchImageSnapshot();
-        // gl.getExtension("STACKGL_destroy_context").destroy();
-        done();
-      });
+  afterAll(() => {
+    gl.getExtension("STACKGL_destroy_context").destroy();
+    gl256.getExtension("STACKGL_destroy_context").destroy();
+  });
+
+  test("It should have a render function", () => {
+    expect(typeof SRE.render).toBe("function");
+  });
+
+  test("It should be able to render rects", done => {
+    const Scene = props =>
+      Rect(
+        {
+          x: 32,
+          y: 32,
+          z: 0,
+          width: 64
+        },
+        []
+      );
+    SRE.init(gl, VIEWPORT_WIDTH, VIEWPORT_HEIGHT, Scene);
+
+    makeSnapshot(gl, VIEWPORT_WIDTH, VIEWPORT_HEIGHT).then(ss => {
+      expect(ss).toMatchImageSnapshot();
+      done();
     });
   });
 
@@ -62,86 +61,75 @@ describe("render engine", () => {
 
       makeSnapshot(gl, VIEWPORT_WIDTH, VIEWPORT_HEIGHT).then(ss => {
         expect(ss).toMatchImageSnapshot();
-        // gl.getExtension("STACKGL_destroy_context").destroy();
         done();
       });
     });
 
-    test("It should take a viewport width and height", done => {
-      const gl = makeGL(VIEWPORT_WIDTH * 2, VIEWPORT_HEIGHT * 2);
+    test("init should take a viewport width and height", done => {
+      // const gl = makeGL(VIEWPORT_WIDTH * 2, VIEWPORT_HEIGHT * 2);
 
       const Scene = props =>
         Rect({ x: 192, y: 0, z: 0, width: 64, children: [] });
-      SRE.init(gl, VIEWPORT_WIDTH * 2, VIEWPORT_HEIGHT * 2, Scene);
+      SRE.init(gl256, VIEWPORT_WIDTH * 2, VIEWPORT_HEIGHT * 2, Scene);
 
-      makeSnapshot(gl, VIEWPORT_WIDTH * 2, VIEWPORT_HEIGHT * 2).then(ss => {
+      makeSnapshot(gl256, VIEWPORT_WIDTH * 2, VIEWPORT_HEIGHT * 2).then(ss => {
         expect(ss).toMatchImageSnapshot();
-        // gl.getExtension("STACKGL_destroy_context").destroy();
         done();
       });
     });
   });
 
-  describe("Scene", () => {
-    test("init should take a scene and render a black background", done => {
-      const Scene = props => {};
-      SRE.init(gl, VIEWPORT_WIDTH, VIEWPORT_HEIGHT, Scene);
+  test("init should take a scene and render a black background", done => {
+    const Scene = props => {};
+    SRE.init(gl, VIEWPORT_WIDTH, VIEWPORT_HEIGHT, Scene);
 
-      makeSnapshot(gl, VIEWPORT_WIDTH, VIEWPORT_HEIGHT).then(ss => {
-        expect(ss).toMatchImageSnapshot();
-        // gl.getExtension("STACKGL_destroy_context").destroy();
-        done();
-      });
+    makeSnapshot(gl, VIEWPORT_WIDTH, VIEWPORT_HEIGHT).then(ss => {
+      expect(ss).toMatchImageSnapshot();
+      done();
     });
+  });
 
-    test("It should render what is in the scene", done => {
-      // const gl = makeGL(VIEWPORT_WIDTH, VIEWPORT_HEIGHT);
+  test("It should render what is in the scene", done => {
+    const Scene = props =>
+      Rect(
+        {
+          x: 32,
+          y: 32,
+          z: 0,
+          width: 64
+        },
+        []
+      );
 
-      const Scene = props =>
-        Rect(
-          {
-            x: 32,
-            y: 32,
-            z: 0,
-            width: 64
-          },
-          []
-        );
+    SRE.init(gl, VIEWPORT_WIDTH, VIEWPORT_HEIGHT, Scene);
 
-      SRE.init(gl, VIEWPORT_WIDTH, VIEWPORT_HEIGHT, Scene);
-
-      makeSnapshot(gl, VIEWPORT_WIDTH, VIEWPORT_HEIGHT).then(ss => {
-        expect(ss).toMatchImageSnapshot();
-        // gl.getExtension("STACKGL_destroy_context").destroy();
-        done();
-      });
+    makeSnapshot(gl, VIEWPORT_WIDTH, VIEWPORT_HEIGHT).then(ss => {
+      expect(ss).toMatchImageSnapshot();
+      done();
     });
+  });
 
-    test("It should move object when position changes from one render to another", done => {
-      let calls = -1;
-      const Scene = props => {
-        calls = calls + 1;
-        console.log(calls);
-        return Rect(
-          {
-            x: 32 + calls * 32,
-            y: 32,
-            z: 0,
-            width: 64
-          },
-          []
-        );
-      };
+  test("It should move object when position changes from one render to another", done => {
+    let calls = -1;
+    const Scene = props => {
+      calls = calls + 1;
+      return Rect(
+        {
+          x: 32 + calls * 32,
+          y: 32,
+          z: 0,
+          width: 64
+        },
+        []
+      );
+    };
 
-      SRE.init(gl, VIEWPORT_WIDTH, VIEWPORT_HEIGHT, Scene);
-      requestAnimationFrame.step();
+    SRE.init(gl, VIEWPORT_WIDTH, VIEWPORT_HEIGHT, Scene);
+    requestAnimationFrame.step();
 
-      makeSnapshot(gl, VIEWPORT_WIDTH, VIEWPORT_HEIGHT).then(ss => {
-        expect(ss).toMatchImageSnapshot();
-        // gl.getExtension("STACKGL_destroy_context").destroy();
-        done();
-      });
-      // });
+    makeSnapshot(gl, VIEWPORT_WIDTH, VIEWPORT_HEIGHT).then(ss => {
+      expect(ss).toMatchImageSnapshot();
+      done();
     });
   });
 });
