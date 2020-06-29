7e6fdda1200cc2880cdc8a1bf58da16a6cd728dd
Arnaud Boeglin <a.boeglin67@gmail.com>
2020-06-29 13:35:15 +0200

    Add alias __Node__ for Node export

--- src/index.js
+++ src/index.js
@@ -3,9 +3,12 @@ import { initRenderer } from "./renderer/renderer";
 import { Node } from "./node";
 import Rect from "./nodes/rect";
 
+const __Node__ = Node;
+
 export {
   initWithRenderer,
   initRenderer,
   Node,
+  __Node__,
   Rect,
 };
