1838af5782cdfe17169b466727405357e059fad1
Arnaud Boeglin <a.boeglin67@gmail.com>
2020-07-27 09:09:32 +0200

    Add withDirection Higher Order Node to snake demo

--- demo/snake/index.js
+++ demo/snake/index.js
@@ -10,10 +10,34 @@ const gl = canvas.getContext("webgl");
 const render = initRenderer({ gl, width, height });
 const run = initWithRenderer(canvas, render);
 
+const Directions = {
+  UP: "UP",
+  DOWN: "DOWN",
+  LEFT: "LEFT",
+  RIGHT: "RIGHT",
+};
+
+const withDirection = Node => (props, { onGlobalKeyDown, state = Directions.UP, setState }) => {
+  onGlobalKeyDown(e => {
+    if (e.key === "ArrowDown" && state !== Directions.UP) {
+      setState(Directions.DOWN);
+    } else if (e.key === "ArrowUp" && state !== Directions.DOWN) {
+      setState(Directions.UP);
+    } else if (e.key === "ArrowLeft" && state !== Directions.RIGHT) {
+      setState(Directions.LEFT);
+    } else if (e.key === "ArrowRight" && state !== Directions.LEFT) {
+      setState(Directions.RIGHT);
+    }
+  });
+
+  return <Node {...props} direction={state} />;
+};
+
 const getT0 = once((t) => t);
 
-const Snake = ({ time }, { setState, state = { tailLength: 3 }, onGlobalKeyDown }) =>{
-  onGlobalKeyDown(console.log);
+
+const Snake = ({ time, direction }, { setState, state = { tailLength: 3 } }) =>{
+  console.log(direction);
   return pipe(
     (t) => t - getT0(t),
     (t) => Math.floor((t * Math.sqrt(state.tailLength)) / 1500) * 20,
@@ -27,7 +51,7 @@ const TailPiece = ({ position }) => (
   <Rect x={position.x} y={position.y} z={0} width={18} height={18} />
 );
 
-const SnakeWithTime = withClock(Date.now, Snake);
+const SnakeWithTime = withClock(Date.now, withDirection(Snake));
 
 const Scene = () => <SnakeWithTime />;
 
